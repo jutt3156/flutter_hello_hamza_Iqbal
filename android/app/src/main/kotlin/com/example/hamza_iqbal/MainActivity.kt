8c1e071a796e3f156eb026d250e577735b99763b
jutt3156 <hamza.iqbaljutt3156@gmail.com>
2021-05-14 14:16:42 +0500

    Work Accomplish

---
+++ android/app/src/main/kotlin/com/example/hamza_iqbal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hamza_iqbal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Hamza Iqbal" + "hamza.iqbaljutt3156@gmail.com"),
+      ),
+    );
+  }
+}
